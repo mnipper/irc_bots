390025c4fea238a4f0e8d136c962cc44f8964924
mnipper <mjn4406@gmail.com>
2013-02-05 17:09:32 -0500

    Add timebomb plugin

--- bot.rb
+++ bot.rb
@@ -1,6 +1,5 @@
 require 'cinch'
 require './creds.rb'
-require 'cinch/plugins/timebomb'
 require 'cinch/plugins/last_seen'
 Dir["./bots/*.rb"].each { |file| require file }
 
@@ -10,6 +9,7 @@ bot = Cinch::Bot.new do
     c.password = Creds.password
     c.nick = Creds.nick
     c.channels = Creds.channels
+    c.plugins.options[Cinch::Plugins::Timebomb] = {channels: Creds.channels }
     c.plugins.plugins = [GoogleBot, HelpBot, UrbanDictionary, SlapBot, CowsayBot, OpBot, AsciiBot, RouletteBot, SpiderMonitor, RageQuit, RedditBot, Cinch::Plugins::Timebomb, Cinch::Plugins::LastSeen]
   end
 
